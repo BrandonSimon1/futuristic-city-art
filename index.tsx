65ff9f8348b524ffe29e3e8725d7b178b2dccc27
BrandonSimon1 <brandon.n.simon@gmail.com>
2020-12-14 23:58:59 -0600

    have background

--- index.tsx
+++ index.tsx
@@ -1,47 +1,67 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
-import Hello from './Hello';
-import './style.css';
+import React, { Component } from "react";
+import { render } from "react-dom";
+import Hello from "./Hello";
+import "./style.css";
 
-interface AppProps { }
+interface AppProps {}
 interface AppState {
   name: string;
 }
 
-const makePoints = (size: number) => Array.from(new Array(size), (x, i) => i).flatMap(i => Array.from(new Array(size), (x, k) => k).map(k => [i, k]))
+const makePoints = (size: number) =>
+  Array.from(new Array(size), (x, i) => i).flatMap(i =>
+    Array.from(new Array(size), (x, k) => k).map(k => ({ x: i, y: k }))
+  );
 
-const getColor = (point: [number, number]): string => 'red'
+const colors = {
+  sun: "#fd9e0b",
+  background: "#db1dca",
+  building: "#600b1c"
+};
+
+const createScene = (): any[] => {
+  return;
+};
+
+const getColor = (() => {
+  const scene = createScene();
+  return ({ x: x0, y: y0 }: { x: number; y: number }): string => {
+    return scene.find(({ x, y }) => x == x0 && y == y0).color;
+  };
+})();
 
 class App extends Component<AppProps, AppState> {
   constructor(props) {
     super(props);
     this.state = {
-      name: 'React'
+      name: "React"
     };
   }
 
   render() {
     return (
-      <svg width="100vw" height="100vh" preserveAspectRatio="none" viewBox="0 0 100 100">
-        {
-          makePoints(200)
-          .map(([x, y]) => ({x, y, color: getColor([x, y])}))
-          .map(
-            ({x, y, color}) =>
-              <rect
-                x={x} 
-                y={y} 
-                width="1" 
-                height="1" 
-                fill={color} 
-                stroke="white"
-                stroke-width=".1"
-              />
-          )
-        }
+      <svg
+        width="100vw"
+        height="100vh"
+        preserveAspectRatio="none"
+        viewBox="0 0 100 100"
+      >
+        {makePoints(100)
+          .map(({ x, y }) => ({ x, y, color: getColor({ x, y }) }))
+          .map(({ x, y, color }) => (
+            <rect
+              x={x}
+              y={y}
+              width="1"
+              height="1"
+              fill={color}
+              stroke="white"
+              stroke-width=".1"
+            />
+          ))}
       </svg>
     );
   }
 }
 
-render(<App />, document.getElementById('root'));
+render(<App />, document.getElementById("root"));
